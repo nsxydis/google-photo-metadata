e691b4f9bfe3fd1d88d40632e3a41070cba16b4a
Nick Xydis <nsxydis@gmail.com>
2025-08-25 15:55:43 -0500

    Added a scan and the results to count all the keys in the metadata

--- tests/scan_metadata.py
+++ tests/scan_metadata.py
@@ -3,6 +3,7 @@ Purpose: Scan through all the metadata files and check what relevant fields ther
 '''
 # Standard module import
 import os
+import json
 
 # Get the default folder
 fol = os.getenv('PHOTO_FOLDER')
@@ -12,8 +13,11 @@ def main():
     if fol is None:
         raise Exception("ERROR: You must provide a folder by setting the PHOTO_FOLDER env variable!")
     
+    # Init a dictionary
+    key_count = {}
+    
     # Loop through all the .json files in the folder
-    for root, dir, files in os.walk(fol):
+    for root, dirs, files in os.walk(fol):
         
         # Loop through all the files
         for file in files:
@@ -22,13 +26,26 @@ def main():
             if not file.endswith('.json'):
                 continue
 
+            # Get the file path
+            path = os.path.join(root, file)
+
             # Look at the file contents
-            # TODO
-            
-            # DEBUG: Look at the file path
-            # print(os.path.join(root, file)) # TEMP
+            with open(path, mode = 'r', encoding = 'utf-8') as f:
+                # Read in the json data
+                data = json.load(f)
 
+                # Increment our key counter
+                for item in data.keys():
+                    try:
+                        key_count[item] += 1
+                    except:
+                        key_count[item] = 1
 
+    # Print out the final results
+    for key, value in key_count.items():
+        print(f'{key}: {value}')
+
+    # NOTE: I found a couple of files with geoDataExif -- these don't seem to contain unique information and can be ignored
 
 
 # Run the main function if this script is called directly
